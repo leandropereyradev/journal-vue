f52d8053426d9035cfb1f979fe5aed25dd4e3d98
Leandro Pereyra <leandro-pereyra@hotmail.es>
2023-08-17 18:27:48 +0200

    Fab on Daybook components

---
+++ src/modules/daybook/components/Fab.vue
@@ -0,0 +1,23 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <button class="btn btn-primary">
+    <i class="fa fa-2x fa-plus"></i>
+  </button>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+button {
+  border-radius: 100%;
+  bottom: 20px;
+  height: 80px;
+  position: fixed;
+  right: 20px;
+  width: 80px;
+}
+</style>
--- src/modules/daybook/views/EntryNonSelected.vue
+++ src/modules/daybook/views/EntryNonSelected.vue
@@ -2,11 +2,16 @@
   <div class="d-flex justify-content-center">
     <h1 class="align-self-center">No hay nada seleccionado</h1>
   </div>
+
+  <Fab />
 </template>
 
 <script>
+import { defineAsyncComponent } from 'vue'
 export default {
-
+  components: {
+    Fab: defineAsyncComponent(() => import('../components/Fab.vue'))
+  }
 }
 </script>
 
