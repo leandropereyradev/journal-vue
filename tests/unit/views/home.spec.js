e1c8cb8a5639b3f692b1287d7dbd02ed9ec1eb22
Leandro Pereyra <leandro-pereyra@hotmail.es>
2023-08-20 12:30:32 +0200

    Test: Home View

---
+++ tests/unit/views/home.spec.js
@@ -0,0 +1,30 @@
+import { shallowMount } from "@vue/test-utils";
+import Home from "@/views/Home";
+
+describe("Home View", () => {
+  test("Should render the component correctly", () => {
+    const wrapper = shallowMount(Home);
+
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  test("Should redirect to no-entry on button click", () => {
+    const mockRouter = {
+      push: jest.fn(),
+    };
+
+    const wrapper = shallowMount(Home, {
+      // global: opciones globales del componente
+      global: {
+        mocks: {
+          $router: mockRouter,
+        },
+      },
+    });
+
+    wrapper.find("button").trigger("click");
+
+    expect(mockRouter.push).toHaveBeenCalled();
+    expect(mockRouter.push).toHaveBeenCalledWith({ name: "non-entry" });
+  });
+});
